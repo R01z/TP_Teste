7d95f21675909bc16b31b1b02de305b010fa2ee3
R01z <igorrt66@gmail.com>
2022-12-10 14:56:05 -0300

    Update NomeUtil.java

--- src/main/java/com/bdq/model/services/util/NomeUtil.java
+++ src/main/java/com/bdq/model/services/util/NomeUtil.java
@@ -3,7 +3,6 @@ package com.bdq.model.services.util;
 public class NomeUtil {
 
     public static boolean nomeValido(String nome) {
-        char letra;
         for (int i = 0; i < nome.length(); i++) {
             letra = nome.charAt(i);
             if (Character.isDigit(letra))
