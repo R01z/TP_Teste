ac5f1254c3db122e439d3d2f8cfd126ff104565f
R01z <igorrt66@gmail.com>
2022-12-10 17:15:48 -0300

    Add excecao a isCPF

--- src/test/java/com/bdq/unittests/CpfUtilTest.java
+++ src/test/java/com/bdq/unittests/CpfUtilTest.java
@@ -8,48 +8,58 @@ import org.junit.Test;
 
 import com.bdq.model.services.util.CpfUtil;
 
-public class CpfUtilTest{
+public class CpfUtilTest {
 
 	@Test
 	public void cpfComTodosOsNumerosIguaisNaoEValido() {
 		boolean resultado = CpfUtil.isCPF("99999999999");
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void cpfComNumeroDeDigitosMenorQue11NaoEValido() {
 		boolean resultado = CpfUtil.isCPF("63735298");
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void cpfComNumeroDeDigitosMaiorQue11NaoEValido() {
 		boolean resultado = CpfUtil.isCPF("6373529885472");
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void cpfComCaracteresNaoNumericosNaoEValido() {
 		boolean resultado = CpfUtil.isCPF("974BRA73598");
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void cpfComoStringVaziaNaoEValido() {
 		boolean resultado = CpfUtil.isCPF("");
 		assertFalse(resultado);
 	}
-	
+
 	@Test
 	public void cpfValido() {
 		boolean resultado = CpfUtil.isCPF("47970631061");
 		assertTrue(resultado);
 	}
-	
+
 	@Test
 	public void imprimeCpfComPontosETraco() {
 		String resultado = CpfUtil.imprimeCPF("47970631061");
-		assertEquals("479.706.310-61",resultado);
+		assertEquals("479.706.310-61", resultado);
+	}
+
+	@Test
+	public void lancaExcecaoCpfNull() {
+		try {
+			boolean resultado = CpfUtil.isCPF(null);
+		} catch (NullPointerException e) {
+			assertEquals("Necessário preencher o CPF", e.getMessage());
+		}
+
 	}
 
 }
