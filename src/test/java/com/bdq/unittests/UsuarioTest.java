eebacce23f7a8660bcfd1dc1263442419f422fc1
R01z <igorrt66@gmail.com>
2022-12-10 15:12:29 -0300

    Atualiza usuario teste

--- src/test/java/com/bdq/unittests/UsuarioTest.java
+++ src/test/java/com/bdq/unittests/UsuarioTest.java
@@ -2,6 +2,7 @@ package com.bdq.unittests;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,11 +17,19 @@ public class UsuarioTest {
 		Usuario professor = new Professor("Professor", 555, "47970631061");
 		assertFalse(professor.getUsrAluno());
 	}
-	
+
 	@Test
 	public void usuarioCadastradoEAluno() {
 		Usuario aluno = new Aluno("Aluno", 555, 2018046, "47970631061");
 		assertTrue(aluno.getUsrAluno());
+	}
+
+	@Test
+	public void setarNomeDoUsuario() {
+		String nome = "Aluno";
+		Usuario aluno = new Aluno("", 555, 2018046, "47970631061");
+		aluno.setNome(nome);
+		assertEquals(nome, aluno.getNome());
 	}
 
 }
