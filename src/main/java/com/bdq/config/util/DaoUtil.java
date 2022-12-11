22f173a040055633d7a9255b45e1801aeb04f9d9
R01z <igorrt66@gmail.com>
2022-12-11 10:03:12 -0300

    Validação de Senha

--- src/main/java/com/bdq/config/util/DaoUtil.java
+++ src/main/java/com/bdq/config/util/DaoUtil.java
@@ -2,6 +2,7 @@ package com.bdq.config.util;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.lang.Character;
 
 public class DaoUtil {
 
@@ -22,5 +23,34 @@ public class DaoUtil {
       return ex.getMessage();
     }
   }
-}
 
+  public static boolean validaSenha(String senha) {
+    if (senha == null)
+      return false;
+
+    if (senha.length() < 8)
+      return false;
+
+    int numeros = 0;
+    int letraMinuscula = 0;
+    int letraMaiuscula = 0;
+    int especial = 0;
+
+    for (int i = 0; i < senha.length(); i++) {
+      char pos = senha.charAt(i);
+      if (Character.isDigit(pos))
+        numeros++;
+      else if (Character.isUpperCase(pos))
+        letraMaiuscula++;
+      else if (Character.isLowerCase(pos))
+        letraMinuscula++;
+      else if (!Character.isLetterOrDigit(pos))
+        especial++;
+    }
+
+    if (numeros == 0 || letraMaiuscula == 0 || letraMinuscula == 0 || especial == 0)
+      return false;
+    else
+      return true;
+  }
+}
--- src/test/java/com/bdq/unittests/DaoUtilTest.java
+++ src/test/java/com/bdq/unittests/DaoUtilTest.java
@@ -15,4 +15,46 @@ public class DaoUtilTest {
         String resposta = DaoUtil.encryptPass("teste");
         assertEquals("46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5", resposta);
     }
+
+    @Test
+    public void senhaInvalidaCasoParametroSejaNull() {
+        boolean resposta = DaoUtil.validaSenha(null);
+        assertFalse(resposta);
+    }
+
+    @Test
+    public void senhaInvalidaCasoParametroSejaMenorQue8() {
+        boolean resposta = DaoUtil.validaSenha("Abc12!");
+        assertFalse(resposta);
+    }
+
+    @Test
+    public void senhaInvalidaCasoParametroNaoTenhaNumero() {
+        boolean resposta = DaoUtil.validaSenha("AbcDEf@!");
+        assertFalse(resposta);
+    }
+
+    @Test
+    public void senhaInvalidaCasoParametroNaoTenhaLetraMaiuscula() {
+        boolean resposta = DaoUtil.validaSenha("1bc23f@!");
+        assertFalse(resposta);
+    }
+
+    @Test
+    public void senhaInvalidaCasoParametroNaoTenhaLetraMinuscula() {
+        boolean resposta = DaoUtil.validaSenha("1BC23F@!");
+        assertFalse(resposta);
+    }
+
+    @Test
+    public void senhaInvalidaCasoParametroNaoTenhaCaractereEspecial() {
+        boolean resposta = DaoUtil.validaSenha("1BC23Fab");
+        assertFalse(resposta);
+    }
+
+    @Test
+    public void senhavalida() {
+        boolean resposta = DaoUtil.validaSenha("1B!23Fab");
+        assertTrue(resposta);
+    }
 }
